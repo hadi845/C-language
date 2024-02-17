103498102cecaa30e105724bf94fb65b87edf9f8
Hadi Muhammad <30381079+hadi845@users.noreply.github.com>
2024-02-17 15:03:14 +0500

    Add files via upload

---
+++ odd if/even/main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int num;
+    printf("Enter a NUMBER to check IF its EVEN or ODD:");
+    scanf("%d",& num);
+    if(num%2==0)
+        printf("%d is an EVEN number",num);
+    if(num%2==1)
+        printf("%d is an ODD number",num);
+}
